74eebb56e5ad9ffa633a39850d81d018c00ab614
Jayant Sharma <jayant.sharma2023@vitstudent.ac.in>
2025-04-30 02:59:22 +0530

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express")
-
+const dotenv = require("dotenv")
 const app = express()
 
+dotenv.config()
 app.get("/home", (req ,res)=>{
     res.json({
         message: "Welcome to the home page" 
     })
 })
-app.listen(3000, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log("Server is running on port 3000")
 })
